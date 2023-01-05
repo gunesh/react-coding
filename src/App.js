961a7918f17b7b84782c142af5798e60d67332ca
gunesh <gunesh26@gmail.com>
2023-01-05 17:03:41 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { split } from 'shlex';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      {console.log(split('ls -al / "dsfsfsf fgfddg gfd fdgfdg "'))}
     </div>
   );
 }
