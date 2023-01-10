928b1953d0c766986f73d17ef7ce00c429f984c7
gunesh <gunesh26@gmail.com>
2023-01-10 15:36:15 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import { split } from 'shlex';
+import Input from './Components/Autosuggest/Input';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
       {console.log(split('ls -al / "dsfsfsf fgfddgg"'))}
+      <Input placeholder="Enter Value" />
     </div>
   );
 }
