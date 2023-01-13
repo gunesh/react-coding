661c3fa7a440287d80a7370d7927823cd35a3642
gunesh <gunesh26@gmail.com>
2023-01-13 11:36:11 +0530

    1 file modified

--- src/Components/Autosuggest/Test.js
+++ src/Components/Autosuggest/Test.js
@@ -95,19 +95,19 @@ const Autocomplete = ({ options }) => {
         </>
       )}
 
-
-<div>
-      <CSSTransition nodeRef={nodeRef} in={inProp} timeout={200} classNames="my-node">
-        <div ref={nodeRef}>
-          {"I'll receive my-node-* classes"}
-        </div>
-      </CSSTransition>
-      <button type="button" onClick={() => setInProp(true)}>
-        Click to Enter
-      </button>
-    </div>
-
-
+      <div>
+        <CSSTransition
+          nodeRef={nodeRef}
+          in={inProp}
+          timeout={2000}
+          classNames="my-node"
+        >
+          <div ref={nodeRef}>{"I'll receive my-node-* classes"}</div>
+        </CSSTransition>
+        <button type="button" onClick={() => setInProp(true)}>
+          Click to Enter
+        </button>
+      </div>
     </Fragment>
   );
 };
