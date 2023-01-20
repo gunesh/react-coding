33e585dafa09c5e8333d00a2718487b3639f9063
gunesh <gunesh26@gmail.com>
2023-01-20 10:39:25 +0530

    2 files modified

--- src/Components/Slide/Slide.js
+++ src/Components/Slide/Slide.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default class MultipleItems extends Component {
+export default class Responsive extends Component {
   render() {
-    const settings = {
-      dots: false,
-      infinite: true,
+    var settings = {
+      dots: true,
+      infinite: false,
       speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     };
     return (
-      <div style={{width:"20px"}}>
-        <h2> Multiple items </h2>
+      <div>
+        <h2> Responsive </h2>
         <Slider {...settings}>
           <div>
             <h3>1</h3>
@@ -37,9 +66,6 @@ export default class MultipleItems extends Component {
           </div>
           <div>
             <h3>8</h3>
-          </div>
-          <div>
-            <h3>9</h3>
           </div>
         </Slider>
       </div>
