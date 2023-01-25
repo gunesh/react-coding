1d0b331d6538967bbb30e7b5f6d2317190f8a5b5
gunesh <gunesh26@gmail.com>
2023-01-25 19:31:05 +0530

    2 files modified

--- src/Components/imageSlider.js
+++ src/Components/imageSlider.js
@@ -5,8 +5,77 @@ import 'react-slideshow-image/dist/styles.css';
 const Example = () => {
   return (
     <div>
-      <Slide slidesToScroll={2} slidesToShow={2} indicators={true}>
-        {/* children here */}
+      <Slide slidesToScroll={2} slidesToShow={4} indicators={false}>
+        <div
+          style={{
+            textAlign: 'center',
+            background: 'red',
+            padding: '200px 0',
+            fontSize: '30px',
+          }}
+        >
+          First Slide
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            background: 'orange',
+            padding: '200px 0',
+            fontSize: '30px',
+          }}
+        >
+          Second Slide
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            background: 'yellow',
+            padding: '200px 0',
+            fontSize: '30px',
+          }}
+        >
+          Third Slide
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            background: 'green',
+            padding: '200px 0',
+            fontSize: '30px',
+          }}
+        >
+          Fourth Slide
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            background: 'blue',
+            padding: '200px 0',
+            fontSize: '30px',
+          }}
+        >
+          Sixth Slide
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            background: 'indigo',
+            padding: '200px 0',
+            fontSize: '30px',
+          }}
+        >
+          Seventh Slide
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            background: 'violet',
+            padding: '200px 0',
+            fontSize: '30px',
+          }}
+        >
+          Eight Slide
+        </div>
       </Slide>
     </div>
   );
