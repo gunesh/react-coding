e74f4f1b0f9abc5b88f3497b3b71072d26a0fb94
gunesh <gunesh26@gmail.com>
2023-02-15 11:11:55 +0530

    2 files modified

--- src/Components/ScrollTopExample.js
+++ src/Components/ScrollTopExample.js
@@ -3,7 +3,20 @@ import React from 'react';
 const ScrollTopExample = () => {
   return (
     <div>
-     ScrollTopExample
+      <div id="container">
+        <div id="scroller">
+          <p>
+            Far out in the uncharted backwaters of the unfashionable end of the
+            western spiral arm of the Galaxy lies a small unregarded yellow sun.
+            Orbiting this at a distance of roughly ninety-two million miles is
+            an utterly insignificant little blue green planet whose
+            ape-descended life forms are so amazingly primitive that they still
+            think digital watches are a pretty neat idea.
+          </p>
+        </div>
+      </div>
+
+      <div id="output">scrollTop: 0</div>
     </div>
   );
 };
